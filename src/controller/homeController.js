866163659662d8436095725e78b522b61a9043b3
Pham Chu Duong <pduong244@gmail.com>
2023-02-14 10:40:45 +0700

    Fix some lines

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -2,13 +2,13 @@ import userService from '../service/userService';
 
 const handleHelloWord = (req, res) => {
     return res.render("home.ejs");
-}
+};
 
 const handleUserPage = async (req, res) => {
     //model => get data from database
     let userList = await userService.getUserList();
     return res.render("user.ejs", { userList });
-}
+};
 
 const handleCreateNewUser = (req, res) => {
     let email = req.body.email;
@@ -18,12 +18,12 @@ const handleCreateNewUser = (req, res) => {
 
     userService.createNewUser(email, password, username);
     return res.redirect("/user");
-}
+};
 
 const handleDelteUser = async (req, res) => {
     await userService.deleteUser(req.params.id);
     return res.redirect("/user");
-}
+};
 
 const getUpdateUserPage = async (req, res) => {
     let id = req.params.id;
@@ -32,7 +32,7 @@ const getUpdateUserPage = async (req, res) => {
     userData = user;
 
     return res.render("user-update.ejs", { userData });
-}
+};
 
 const handleUpdateUser = async (req, res) => {
     let email = req.body.email;
@@ -43,15 +43,15 @@ const handleUpdateUser = async (req, res) => {
 
     return res.redirect("/user");
 
-}
+};
 
 const handleUserGetAPI = async (req, res) => {
     setTimeout(async () => {
         let userList = await userService.getUserList();
         return res.status(200).json(userList);
-    }, 100)
+    }, 100);
 
-}
+};
 
 
 const handleDelteUserAPI = async (req, res) => {
@@ -59,8 +59,8 @@ const handleDelteUserAPI = async (req, res) => {
     return res.status(200).json({
         message: `User with the id = ${req.params.id} is deleted successfully!`,
         errCode: 0
-    })
-}
+    });
+};
 
 const handleCreateNewUserAPI = async (req, res) => {
     let email = req.body.email;
@@ -73,11 +73,11 @@ const handleCreateNewUserAPI = async (req, res) => {
         return res.status(200).json({
             message: `A new User is created successfully!`,
             errCode: 0
-        })
-    }, 5000)
-}
+        });
+    }, 5000);
+};
 
 module.exports = {
     handleHelloWord, handleUserPage, handleCreateNewUser, handleDelteUser, getUpdateUserPage,
     handleUpdateUser, handleUserGetAPI, handleDelteUserAPI, handleCreateNewUserAPI
-}
+};
